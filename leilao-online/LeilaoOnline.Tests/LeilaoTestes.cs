0664d3a55f8b1ef37b19f4ee30a8143eb6ea188c
Douglas Lima <Douglasproglima@gmail.com>
2022-02-09 22:04:18 -0300

    Add new tests

--- leilao-online/LeilaoOnline.Tests/LeilaoTestes.cs
+++ leilao-online/LeilaoOnline.Tests/LeilaoTestes.cs
@@ -11,7 +11,7 @@ namespace LeilaoOnline.Tests
             var leilao = new Leilao("Picasso");
             var paulo = new Interessada("Paulo", leilao);
 
-            leilao.RecebeLance(paulo, 850);
+            leilao.RecebeLance(paulo, 1000);
 
             //Act - Método sob teste
             leilao.TerminarPregao();
@@ -36,6 +36,55 @@ namespace LeilaoOnline.Tests
             leilao.RecebeLance(maria, 998);
             leilao.RecebeLance(douglas, 1000);
             leilao.RecebeLance(maria, 990);
+
+            //Act - Método sob teste
+            leilao.TerminarPregao();
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void TesteLeilaoComQuatroClientes()
+        {
+            //Arrange - Cenário
+            var leilao = new Leilao("Picasso");
+            var paulo = new Interessada("Paulo", leilao);
+            var maria = new Interessada("Maria", leilao);
+            var douglas = new Interessada("Douglas", leilao);
+
+            leilao.RecebeLance(paulo, 100);
+            leilao.RecebeLance(maria, 998);
+            leilao.RecebeLance(douglas, 1000);
+            leilao.RecebeLance(maria, 990);
+            leilao.RecebeLance(douglas, 1050);
+
+            //Act - Método sob teste
+            leilao.TerminarPregao();
+
+            //Assert
+            var valorEsperado = 1050;
+            var valorObtido = leilao.Ganhador.Valor;
+            
+            Assert.Equal(valorEsperado, valorObtido);
+            Assert.Equal(douglas, leilao.Ganhador.Cliente);
+        }
+
+        [Fact]
+        public void TesteLeilaoComLancesOrdenadosPorValor()
+        {
+            //Arrange - Cenário
+            var leilao = new Leilao("Picasso");
+            var paulo = new Interessada("Paulo", leilao);
+            var maria = new Interessada("Maria", leilao);
+            var douglas = new Interessada("Douglas", leilao);
+
+            leilao.RecebeLance(paulo, 100);
+            leilao.RecebeLance(maria, 990);
+            leilao.RecebeLance(maria, 998);
+            leilao.RecebeLance(douglas, 1000);
 
             //Act - Método sob teste
             leilao.TerminarPregao();
