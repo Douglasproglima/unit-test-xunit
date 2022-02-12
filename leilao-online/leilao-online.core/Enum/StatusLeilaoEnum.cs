6a694fb01e59680b8b5a8cbccda271105cb403ed
Douglas Lima <Douglasproglima@gmail.com>
2022-02-12 10:54:25 -0300

    Add StatusLeilao em new problem and BUG Unit Test

---
+++ leilao-online/leilao-online.core/Enum/StatusLeilaoEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace LeilaoOnline.Core.Enum
+{
+    public class StatusLeilaoEnum
+    {
+        public enum StatusLeilao
+        {
+            ANTES_PREGAO = 0,
+            EM_ANDAMENTO = 1,
+            FINALIZADO = 2
+        }
+    }
+}
--- leilao-online/leilao-online.core/Leilao.cs
+++ leilao-online/leilao-online.core/Leilao.cs
@@ -5,29 +5,40 @@ namespace LeilaoOnline.Core
 {
     public class Leilao
     {
+        public enum StatusLeilao
+        {
+            ANTES_PREGAO,
+            EM_ANDAMENTO,
+            FINALIZADO
+        }
+
         private IList<Lance> _lances;
         public IEnumerable<Lance> Lances => _lances;
         public string Peca { get; }
         public Lance Ganhador { get; private set; }
+        public StatusLeilao Status { get; private set; }
 
         public Leilao(string peca)
         {
             Peca = peca;
             _lances = new List<Lance>();
+            Status = StatusLeilao.ANTES_PREGAO;
         }
 
         public void RecebeLance(Interessada cliente, double valor)
         {
-            _lances.Add(new Lance(cliente, valor));
+            if(Status.Equals(StatusLeilao.EM_ANDAMENTO))
+                _lances.Add(new Lance(cliente, valor));
         }
 
         public void IniciaPregao()
         {
-
+            Status = StatusLeilao.EM_ANDAMENTO;
         }
 
         public void TerminarPregao()
         {
+            Status = StatusLeilao.FINALIZADO;
             Ganhador = Lances
                 .DefaultIfEmpty(new Lance(null, 0))
                 .OrderBy(lance => lance.Valor)
