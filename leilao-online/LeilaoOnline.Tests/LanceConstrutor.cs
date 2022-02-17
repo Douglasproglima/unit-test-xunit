277b6a41b395b3155280fe922907decca8d88d62
Douglas Lima <Douglasproglima@gmail.com>
2022-02-16 22:55:09 -0300

    New test negative value

---
+++ leilao-online/LeilaoOnline.Tests/LanceConstrutor.cs
@@ -0,0 +1,26 @@
+﻿using LeilaoOnline.Core;
+using System;
+using Xunit;
+
+namespace LeilaoOnline.Tests
+{
+    public class LanceConstrutor
+    {
+        /* Give: Dado leilão com lance negativo
+         * When: Quando o pregão/leilão estiver iniciado ou em andamento
+         * Then: Então gera exceção
+         */
+        [Fact]
+        public void LancaArgumentExceptionDadoValorNegativo()
+        {
+            //Arrange/Given - Cenário
+            var valorNegativo = -200;
+
+            //Act/When - Método sob teste
+            Assert.Throws<ArgumentException>( () =>
+                //Assert/Then
+                new Lance(null, valorNegativo)
+            );
+        }
+    }
+}
